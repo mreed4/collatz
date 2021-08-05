bcb095e7dd405518eab751062aeec92f4fcf8a56
Matthew <matthew.reed@outlook.com>
2021-08-04 19:59:06 -0700

    Move output to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 const collatz = () => {
-    // Function goes here
+
     let n = Math.floor(Math.random() * 10);
-    console.log(`Initial number: ${n}`);
+
+    let arr = [n];
 
     while (n != 1 && n > 0) {
+
         if (n % 2 === 0) {
             n = n / 2;
-            console.log(n);
+            arr.push(n);
         } else {
             n = (n * 3) + 1;
-            console.log(n);
+            arr.push(n);
         }
+
     }
+
+    console.log(arr);
 }
